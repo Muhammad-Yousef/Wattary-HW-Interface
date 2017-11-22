851232e0c55b7283a52e8b9d7391122d8201d31b
nourhan <nourhansalama29796@yahoo.com>
2017-11-22 16:45:42 +0200

    transmitter of elevator

---
+++ transmitter_of_elevator/transmitter_of_elevator.ino
@@ -0,0 +1,33 @@
+#include <VirtualWire.h>
+char *controller;
+void setup() {
+pinMode(13,OUTPUT);
+vw_set_ptt_inverted(true); //
+vw_set_tx_pin(12);
+vw_setup(4000);// speed of data transfer Kbps
+}
+
+void loop(){
+   switch (Serial.parseInt()){
+    case 1:
+    ledon();
+    break;
+    case 2:
+    ledoff();
+    break;
+   }
+   }
+  void ledon(){
+controller="1" ;
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(13,1);
+  }
+  void ledoff(){
+controller="0" ;
+vw_send((uint8_t *)controller, strlen(controller));
+vw_wait_tx(); // Wait until the whole message is gone
+digitalWrite(13,0);
+  }
+
+
