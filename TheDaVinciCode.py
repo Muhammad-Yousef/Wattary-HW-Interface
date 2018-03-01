4c4147b778ecf407879624f37598e4857c87a9ca
Mohamed Elamir <Mohamed.Elamir3@Gmail.com>
2018-03-01 02:44:34 +0200

    Hardware Integration Code with Brain

---
+++ TheDaVinciCode.py
@@ -0,0 +1,26 @@
+
+lightCodeON = {
+
+    "bedroom":'11',
+    "bathroom":'13',
+    "hallway":'15',
+}
+lightCodeOff = {
+
+    "bedroom":'12',
+    "bathroom":'14',
+    "hallway":'16',
+}
+coffeeCode = {
+
+    "on":'21',
+    "off":'22'
+}
+
+tvCode = {
+
+    "on":'17',
+    "off":'18'
+
+}
+
