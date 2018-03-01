2da3da709664d1ef3587898475e2e54fcf40c97a
Mohamed Elamir <Mohamed.Elamir3@Gmail.com>
2018-03-01 05:29:43 +0200

    Implement TheDaVinciCode
    Implement the Codes that integrating hardware with Wattary Brain
    
    Co-Authored-By: fatemamohamed <fatemamohamed@users.noreply.github.com>

--- receive final py/receive final python.py
+++ receive final py/receive final python.py
@@ -1,16 +1,36 @@
+import os
 import paho.mqtt.client as mqtt
 
 # This is the Subscriber
-
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
   client.subscribe("pi")
-
 def on_message(client, userdata, msg):
-  if msg.payload.decode() == "1":
-    print("Yes!")
+  if msg.payload.decode() == "11":
+    print("light bedroom on ")
+  elif msg.payload.decode() == "12":
+    print("light bedroom off")
+  elif msg.payload.decode() == "13":
+    print("light bathroom on ")
+  elif msg.payload.decode() == "14":
+    print("light bathroom off ")
+  elif msg.payload.decode() == "15":
+    print("light Hall on")
+  elif msg.payload.decode() == "16":
+    print("light Hall off ")
+  elif msg.payload.decode() == "17":
+    print("TV On")
+  elif msg.payload.decode() == "18":
+    print("Tv Off")
+  elif msg.payload.decode() == "21":
+    print("Coffe Machine On")
+  elif msg.payload.decode() == "22":
+    print("Coffe Machine Off")
+  elif msg.payload.decode() == "99":
+    os.system("fswebcam test99.jpeg")
+    print "photo taken"
    # client.disconnect()
-    
+  print("  ")
 client = mqtt.Client()
 client.username_pw_set("incmrvjk", "EUVFYnYcv0Qv")
 client.connect("m14.cloudmqtt.com", 11652, 60)
@@ -20,4 +40,3 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.loop_forever()
-
