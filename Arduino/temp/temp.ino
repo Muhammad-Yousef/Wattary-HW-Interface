96a84cf1f719a824b04f77c67fe3d43ef93cb845
nourhan <nourhansalama29796@yahoo.com>
2018-06-21 13:06:29 +0200

    edit temprature
    add sensor2

--- Arduino/temp/temp.ino
+++ Arduino/temp/temp.ino
@@ -1,6 +1,7 @@
 int val;
 int tempPin = 1;
-
+int tempin2=2;
+int val2;
 void setup()
 {
 Serial.begin(9600);
@@ -12,11 +13,22 @@ float mv = ( val/1024.0)*5000;
 float cel = mv/10;
 float farh = (cel*9)/5 + 32;
 
+
+val2 = analogRead(tempin2);
+float mv2 = ( val2/1024.0)*5000; 
+float cel2 = mv2/10;
+float farh2 = (cel2*9)/5 + 32;
+
 Serial.print("TEMPRATURE = ");
 Serial.print(cel);
 Serial.print("*C");
 Serial.println();
+Serial.print("TEMPRATURE2 = ");
+Serial.print(cel2);
+Serial.print("*C");
+Serial.println();
 delay(1000);
+
 
 /* uncomment this to get temperature in farenhite 
 Serial.print("TEMPRATURE = ");
