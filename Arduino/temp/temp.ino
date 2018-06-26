aa47307eac33ee5f56246814f7b6c2ed52ef5da5
nourhan <nourhansalama29796@yahoo.com>
2018-06-26 14:46:40 +0200

    edit of temprature
    edit of temprature(2 sensor)

--- Arduino/temp/temp.ino
+++ Arduino/temp/temp.ino
@@ -9,24 +9,24 @@ Serial.begin(9600);
 void loop()
 {
 val = analogRead(tempPin);
+val2 = analogRead(tempin2);
 float mv = ( val/1024.0)*5000; 
 float cel = mv/10;
 float farh = (cel*9)/5 + 32;
 
 
-val2 = analogRead(tempin2);
-float mv2 = ( val2/1024.0)*5000; 
-float cel2 = mv2/10;
-float farh2 = (cel2*9)/5 + 32;
+//float mv2 = ( val2/1024.0)*5000; 
+//float cel2 = mv2/10;
+//float farh2 = (cel2*9)/5 + 32;
 
 Serial.print("TEMPRATURE = ");
 Serial.print(cel);
 Serial.print("*C");
 Serial.println();
-Serial.print("TEMPRATURE2 = ");
+/*Serial.print("TEMPRATURE2 = ");
 Serial.print(cel2);
 Serial.print("*C");
-Serial.println();
+Serial.println();*/
 delay(1000);
 
 
