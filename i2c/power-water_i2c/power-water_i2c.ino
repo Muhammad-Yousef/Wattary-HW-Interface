58ea3fbcde2bb38c451c51120211e59f95d7be0d
nourhan <nourhansalama29796@yahoo.com>
2018-07-02 01:18:13 +0200

    power-water i2c
    power-water i2c

---
+++ i2c/power-water_i2c/power-water_i2c.ino
@@ -0,0 +1,253 @@
+/*
+I2C Pinouts
+SDA -> A4
+SCL -> A5
+*/
+
+//Import the library required 
+#include <Wire.h>
+
+
+int currentPin = 1;              //Assign CT input
+double kilos = 0;
+int peakPower = 0;
+
+
+
+byte statusLed    = 13;
+
+byte sensorInterrupt = 0;  // 0 = digital pin 2
+byte sensorPin       = 2;
+
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
+float calibrationFactor = 4.5;
+
+volatile byte pulseCount;  
+
+float flowRate;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+
+unsigned long oldTime;
+
+
+
+
+
+
+
+
+
+
+//Slave Address for the Communication
+#define SLAVE_ADDRESS 0x05
+
+char number[50];
+int state = 0;
+
+//Code Initialization
+void setup() {
+  // initialize i2c as slave
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDRESS);
+ // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+
+
+
+
+  pinMode(statusLed, OUTPUT);
+  digitalWrite(statusLed, HIGH);  // We have an active-low LED attached
+  
+  pinMode(sensorPin, INPUT);
+  digitalWrite(sensorPin, HIGH);
+
+  pulseCount        = 0;
+  flowRate          = 0.0;
+  flowMilliLitres   = 0;
+  totalMilliLitres  = 0;
+  oldTime           = 0;
+
+  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
+  // Configured to trigger on a FALLING state change (transition from HIGH
+  // state to LOW state)
+  attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+
+
+
+  
+}
+
+void loop() {
+
+int current = 0;
+  int maxCurrent = 0;
+  int minCurrent = 1000;
+  for (int i=0 ; i<=200 ; i++)  //Monitors and logs the current input for 200 cycles to determine max and min current
+  {
+    current = analogRead(currentPin);    //Reads current input
+    if(current >= maxCurrent)
+      maxCurrent = current;
+    else if(current <= minCurrent)
+      minCurrent = current;
+  }
+  if (maxCurrent <= 517)
+  {
+    maxCurrent = 516;
+  }
+  double RMSCurrent = ((maxCurrent - 516)*0.707)/11.8337;    //Calculates RMS current based on maximum value
+  int RMSPower = 220*RMSCurrent;    //Calculates RMS Power Assuming Voltage 220VAC, change to 110VAC accordingly
+  if (RMSPower > peakPower)
+  {
+    peakPower = RMSPower;
+  }
+  kilos = kilos + (RMSPower * (2.05/60/60/1000));    //Calculate kilowatt hours used
+  delay (2000);
+
+
+
+    
+
+
+
+
+
+
+  
+
+  if((millis() - oldTime) > 1000)    // Only process counters once per second
+  { 
+    // Disable the interrupt while calculating flow rate and sending the value to
+    // the host
+    detachInterrupt(sensorInterrupt);
+        
+    // Because this loop may not complete in exactly 1 second intervals we calculate
+    // the number of milliseconds that have passed since the last execution and use
+    // that to scale the output. We also apply the calibrationFactor to scale the output
+    // based on the number of pulses per second per units of measure (litres/minute in
+    // this case) coming from the sensor.
+    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+    
+    // Note the time this processing pass was executed. Note that because we've
+    // disabled interrupts the millis() function won't actually be incrementing right
+    // at this point, but it will still return the value it was set to just before
+    // interrupts went away.
+    oldTime = millis();
+    
+    // Divide the flow rate in litres/minute by 60 to determine how many litres have
+    // passed through the sensor in this 1 second interval, then multiply by 1000 to
+    // convert to millilitres.
+    flowMilliLitres = (flowRate / 60) * 1000;
+    
+    // Add the millilitres passed in this second to the cumulative total
+    totalMilliLitres += flowMilliLitres;
+      
+    unsigned int frac;
+    
+    // Print the flow rate for this second in litres / minute
+   
+    
+
+    // Reset the pulse counter so we can start incrementing again
+    pulseCount = 0;
+    
+    // Enable the interrupt again now that we've finished sending output
+    attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+  }
+}
+
+/*
+Insterrupt Service Routine
+ */
+void pulseCounter()
+{
+  // Increment the pulse counter
+  pulseCount++;
+
+
+
+
+
+
+
+
+
+  
+
+  
+
+
+
+  
+  delay(100);
+} // end loop
+
+// callback for received data
+void receiveData(int byteCount){
+  int i = 0;
+  while(Wire.available()) { 
+    number[i] = Wire.read();
+    i++;
+  }
+  number[i] = '\0';
+  Serial.print(number);
+}  // end while
+
+// callback for sending data
+void sendData(){
+  Wire.write( ( RMSCurrent) ,("A"),
+  //Serial.setCur(10,0),(RMSPower),("W"),
+  //Serial.setCursor(0,1);
+  (kilos),("kWh"),
+  //Serial.setCursor(10,1);
+  (peakPower),("W"),( current),("a"),
+  ("Flow rate: "),(int(flowRate)),("L/min"),("\t"),("Output Liquid Quantity: "), (totalMilliLitres),("mL"),  ("\t"),(totalMilliLitres/1000),("L");
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  );
+
+
+
+
+
+  int current = 0;
+  int maxCurrent = 0;
+  int minCurrent = 1000;
+  for (int i=0 ; i<=200 ; i++)  //Monitors and logs the current input for 200 cycles to determine max and min current
+  {
+    current = analogRead(currentPin);    //Reads current input
+    if(current >= maxCurrent)
+      maxCurrent = current;
+    else if(current <= minCurrent)
+      minCurrent = current;
+  }
+  if (maxCurrent <= 517)
+  {
+    maxCurrent = 516;
+  }
+  double RMSCurrent = ((maxCurrent - 516)*0.707)/11.8337;    //Calculates RMS current based on maximum value
+  int RMSPower = 220*RMSCurrent;    //Calculates RMS Power Assuming Voltage 220VAC, change to 110VAC accordingly
+  if (RMSPower > peakPower)
+  {
+    peakPower = RMSPower;
+  }
+  kilos = kilos + (RMSPower * (2.05/60/60/1000));    //Calculate kilowatt hours used
+  delay (2000);
+
+
+
+    
+}
+
+//End of the program
